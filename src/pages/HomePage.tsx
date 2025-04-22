487f2bf558dbbeb6b5944a4914b1664919aec869
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-04-22 11:15:28 +0530

    add router

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-function App() {
+function HomePage() {
   return (
     <>
       <h1>Hello From MernSpace</h1>
@@ -6,4 +6,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage;
