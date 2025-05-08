4b93eb3f2d197013b90f5429160e1eaa138a1bf4
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-05-08 21:13:00 +0530

    add users table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,14 +1,75 @@
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Space, Table } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getUsers } from "../../http/api";
+import { User } from "../../types";
+
+const columns = [
+  { title: "ID", key: "id", dataIndex: "id" },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
 
 const Users = () => {
+  const {
+    data: users,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => getUsers().then((res) => res.data),
+  });
   return (
     <>
-      <Breadcrumb
-        separator={<RightOutlined />}
-        items={[{ title: <Link to={"/"}>Dashboard</Link> }, { title: "Users" }]}
-      />
+      <Space direction="vertical" size={"large"} style={{ width: "100%" }}>
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[
+            { title: <Link to={"/"}>Dashboard</Link> },
+            { title: "Users" },
+          ]}
+        />
+        {isLoading && <div>Loading</div>}
+        {isError && <div>{error.message}</div>}
+        <Table dataSource={users} columns={columns} />
+      </Space>
+      {/* {users && (
+        <div>
+          <h1>Users</h1>
+          <ul>
+            {users.map((user: User) => (
+              <li key={user.id}>{user.firstName}</li>
+            ))}
+          </ul>
+        </div>
+      )} */}
     </>
   );
 };
