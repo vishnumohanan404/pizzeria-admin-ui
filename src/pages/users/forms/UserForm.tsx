fa54204180bf5b0b720401088c0fffe036440797
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-05-09 19:10:17 +0530

    add user form fields

--- src/pages/users/forms/UserForm.tsx
+++ src/pages/users/forms/UserForm.tsx
@@ -1,23 +1,84 @@
-import { Card, Col, Form, Input, Row } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Col, Form, Input, Row, Select, Space } from "antd";
+import { getTenants } from "../../../http/api";
+import { Tenant } from "../../../types";
 
 const UserForm = () => {
+  const { data: tenants } = useQuery({
+    queryKey: ["tenants"],
+    queryFn: () => {
+      return getTenants().then((res) => res.data);
+    },
+  });
   return (
     <Row>
       <Col span={24}>
-        <Card title="Basic Info" variant="borderless">
-          <Row gutter={20}>
-            <Col span={12}>
-              <Form.Item label="First Name" name="firstName">
-                <Input />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item label="Last Name" name="lastName">
-                <Input />
-              </Form.Item>
-            </Col>
-          </Row>
-        </Card>
+        <Space direction="vertical" size={"large"}>
+          <Card title="Basic Info" variant="borderless">
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item label="First Name" name="firstName">
+                  <Input size="large" />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Last Name" name="lastName">
+                  <Input size="large" />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Email" name="email">
+                  <Input size="large" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+          <Card title="Security Info" variant="borderless">
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item label="Password" name="password">
+                  <Input size="large" type="password" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+          <Card title="Role" variant="borderless">
+            <Row gutter={20}>
+              <Col span={12}>
+                <Form.Item label="Role" name="role">
+                  <Select
+                    style={{ width: "100%" }}
+                    allowClear={true}
+                    onChange={() => {}}
+                    placeholder="Select Role"
+                    size="large"
+                  >
+                    <Select.Option value="admin">Admin</Select.Option>
+                    <Select.Option value="manager">Manager</Select.Option>
+                    <Select.Option value="customer">Customer</Select.Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Restaurant" name="tenantId">
+                  <Select
+                    style={{ width: "100%" }}
+                    allowClear={true}
+                    onChange={() => {}}
+                    placeholder="Select Restaurant"
+                    size="large"
+                  >
+                    {tenants?.map((tenant: Tenant) => (
+                      <Select.Option key={tenant.id} value={tenant.id}>
+                        {tenant.name}
+                      </Select.Option>
+                    ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+        </Space>
       </Col>
     </Row>
   );
