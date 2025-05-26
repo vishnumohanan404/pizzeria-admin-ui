ef695f1d519fc8e6d8b1629af57e9511ee96f604
Vishnu Mohan <77823657+KingKong26@users.noreply.github.com>
2025-05-26 22:33:16 +0530

    add api gateway

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,14 +1,18 @@
 import { CreateUserData, Credentials } from "../types";
 import { api } from "./client";
 
+export const AUTH_SERVICE = "/api/auth";
+export const CATALOG_SERVICE = "/api/catalog";
+
 // Auth service
 export const login = (credentials: Credentials) =>
-  api.post("/auth/login", credentials);
-export const self = () => api.get("/auth/self");
-export const logout = () => api.post("/auth/logout");
+  api.post(`${AUTH_SERVICE}/auth/login`, credentials);
+export const self = () => api.get(`${AUTH_SERVICE}/auth/self`);
+export const logout = () => api.post(`${AUTH_SERVICE}/auth/logout`);
 export const getUsers = (queryString: string) =>
-  api.get(`/users?${queryString}`);
-export const getTenants = () => api.get("/tenants");
-export const createUser = (user: CreateUserData) => api.post("/users", user);
+  api.get(`${AUTH_SERVICE}/users?${queryString}`);
+export const getTenants = () => api.get(`${AUTH_SERVICE}/tenants`);
+export const createUser = (user: CreateUserData) =>
+  api.post(`${AUTH_SERVICE}/users`, user);
 export const updateUser = (user: CreateUserData, id: string) =>
-  api.patch(`/users/${id}`, user);
+  api.patch(`${AUTH_SERVICE}/users/${id}`, user);
